059200b786cf4426444ff8bb38f6d422f96a69f5
Aʙɪsʜɪᴋᴛʜ Jᴏs <73195851+abishikth-jos@users.noreply.github.com>
2025-04-09 12:15:51 +0400

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -98,7 +98,7 @@ const Header = () => {
           onClick={scrollTop}
           className='nav__logo capitalize tracking-[0.05em] dark:text-titleColorDark text-titleColor'
         >
-          My Portfolio
+          TrackStudio
         </Link>
         <div
           className={`${
