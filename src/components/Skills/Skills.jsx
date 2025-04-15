8eedb7aa63eab6a50003a706c96160c137764b5c
Aʙɪsʜɪᴋᴛʜ Jᴏs <73195851+abishikth-jos@users.noreply.github.com>
2025-04-15 12:30:07 +0400

    Update Skills.jsx

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -16,12 +16,12 @@ const Skills = () => {
       id='skills'
     >
       <h2 className='uppercase tracking-[0.05em] text-[34px] sm:text-h1Size font-[700] text-titleColor dark:text-titleColorDark text-center'>
-        Professional Skills
+        Telegram
       </h2>
       <p className='leading-[1.7] text-primaryColor uppercase text-smaller font-[700] mb-[60px] text-center'>
-        My
+        Tele
         <span className='font-[Caveat] text-titleColor dark:text-titleColorDark text-h3Size capitalize ml-[10px]'>
-          Talent
+          Zone
         </span>
       </p>
 
